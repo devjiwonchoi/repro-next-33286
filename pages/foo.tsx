f7e6a37bcecc9a4362e54ac9431e7f2b43e24b21
Jiwon Choi <devjiwonchoi@gmail.com>
2024-08-01 12:32:03 +0000

    remove legacy link behavior

--- pages/foo.tsx
+++ pages/foo.tsx
@@ -1,13 +1,9 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
-const DynamicImportRedButton = dynamic<{}>(
-  () => import('@/components/red').then((module) => module.Red),
-  {
-    loading: () => <div>Loading...</div>,
-    ssr: false,
-  }
-)
+const DynamicImportRedButton = dynamic<{}>(() =>
+  import("@/components/red").then((module) => module.Red),
+);
 
 export default function Foo() {
-  return <DynamicImportRedButton />
+  return <DynamicImportRedButton />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import { Red } from '@/components/red'
-import Link from 'next/link'
+import { Red } from "@/components/red";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <Link href="/foo">
-        <a>/foo</a>
-      </Link>
+      <Link href="/foo">/foo</Link>
       {/* This Red should be imported to be reproduced */}
       <Red />
     </>
-  )
+  );
 }
