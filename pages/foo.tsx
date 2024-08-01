e4cedde66ba9a47cb0930b42fea73eeb960bb5ce
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-01 20:42:49 +0900

    remove unnecessary from foo

--- pages/foo.tsx
+++ pages/foo.tsx
@@ -7,32 +7,7 @@ const DynamicImportRedButton = dynamic<{}>(
     ssr: false,
   }
 )
-const Foo = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        padding: '5vw',
-      }}
-    >
-      <h1>Foo Page</h1>
-      <p>
-        If you load this page directly, the button should be red. If you load
-        one of the routes, then click the link to this page, then click the link
-        to this page, one of the buttons will have a gray background (only when
-        doing yarn build)
-      </p>
-      <p>
-        The red and green buttons are dynamically imported. The blue button is
-        not
-      </p>
-      <DynamicImportRedButton />
-    </div>
-  )
-}
 
-export default Foo
+export default function Foo() {
+  return <DynamicImportRedButton />
+}
